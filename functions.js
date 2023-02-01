a81dee4c2f1699b18edb9b61c5d1eeb16319934d
MartinAcosta-dev <martinacostadev@gmail.com>
2023-02-01 11:48:49 -0300

    Juego funcionando

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 var marcaNula = "";
 var marcaActual = "X";
+var clickCounter = 0;
 
 function cambiarMarca(){
     if(marcaActual=="X"){
@@ -7,4 +8,124 @@ function cambiarMarca(){
     }else{
         marcaActual = "X";
     }
+
+    clickCounter++;
 }
+
+
+function ganoX(grid){
+    //Corrobora el vector y se fija si ganó X
+    let resultado = false;
+    let bCorte = false;
+
+    let i = 0;
+    let j = 0;
+
+    //Vemos vertical -----------------------------------------------------------------
+    if( ( grid[0][0] == "X" ) && ( grid[1][0] == "X" ) && ( grid[2][0] == "X" ) ){
+        resultado = true;
+    }
+
+    if( ( grid[0][1] == "X" ) && ( grid[1][1] == "X" ) && ( grid[2][1] == "X" ) ){
+        resultado = true;
+    }
+
+    if( ( grid[0][2] == "X" ) && ( grid[1][2] == "X" ) && ( grid[2][2] == "X" ) ){
+        resultado = true;
+    }
+
+    //Vemos horizontal ---------------------------------------------------------------
+    if( ( grid[0][0] == "X" ) && ( grid[0][1] == "X" ) && ( grid[0][2] == "X" ) ){
+        resultado = true;
+    }
+
+    if( ( grid[1][0] == "X" ) && ( grid[1][1] == "X" ) && ( grid[1][2] == "X" ) ){
+        resultado = true;
+    }
+
+    if( ( grid[2][0] == "X" ) && ( grid[2][1] == "X" ) && ( grid[2][2] == "X" ) ){
+        resultado = true;
+    }
+
+    //Vemos cruzado ------------------------------------------------------------------
+    if( grid[1][1] == "X" ){
+        if( ( grid[0][0] == "X" ) && ( grid[2][2] == "X" ) ){
+            resultado = true;
+        }else if( ( grid[0][2] == "X" ) && ( grid[2][0] == "X" )){
+            resultado = true;
+        }
+    }
+
+
+    return resultado;
+}
+
+function ganoO(grid){
+    //Corrobora el vector y se fija si ganó X
+    let resultado = false;
+    let bCorte = false;
+
+    let i = 0;
+    let j = 0;
+
+    //Vemos vertical -----------------------------------------------------------------
+    if( ( grid[0][0] == "O" ) && ( grid[1][0] == "O" ) && ( grid[2][0] == "O" ) ){
+        resultado = true;
+    }
+
+    if( ( grid[0][1] == "O" ) && ( grid[1][1] == "O" ) && ( grid[2][1] == "O" ) ){
+        resultado = true;
+    }
+
+    if( ( grid[0][2] == "O" ) && ( grid[1][2] == "O" ) && ( grid[2][2] == "O" ) ){
+        resultado = true;
+    }
+
+    //Vemos horizontal ---------------------------------------------------------------
+    if( ( grid[0][0] == "O" ) && ( grid[0][1] == "O" ) && ( grid[0][2] == "O" ) ){
+        resultado = true;
+    }
+
+    if( ( grid[1][0] == "O" ) && ( grid[1][1] == "O" ) && ( grid[1][2] == "O" ) ){
+        resultado = true;
+    }
+
+    if( ( grid[2][0] == "O" ) && ( grid[2][1] == "O" ) && ( grid[2][2] == "O" ) ){
+        resultado = true;
+    }
+
+    //Vemos cruzado ------------------------------------------------------------------
+    if( grid[1][1] == "O" ){
+        if( ( grid[0][0] == "O" ) && ( grid[2][2] == "O" ) ){
+            resultado = true;
+        }else if( ( grid[0][2] == "O" ) && ( grid[2][0] == "O" )){
+            resultado = true;
+        }
+    }
+
+
+    return resultado;
+}
+
+function showWinMesssagge(ganador){
+    $("#dialog").dialog({
+        modal: true,
+        draggable: false,
+        width: 500,
+        dialogClass: "no-close",
+        buttons: [
+                    {
+                        text: "OK",
+                        click: function(){$( this ).dialog( "close" )}         
+                    }
+                ]
+    }); 
+
+    let parrafo = $("#parrafoVictoria");
+    if(ganador == "Empate"){
+        parrafo.text("Ha habido un empate");
+    }else{
+        parrafo.text("Ha ganado "+ganador+"!!!");
+    }
+}
+     
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    let grid = [["-","-","-"],["-","-","-"],["-","-","-"]];
+    let grid = [[marcaNula, marcaNula, marcaNula],[marcaNula,marcaNula,marcaNula],[marcaNula,marcaNula,marcaNula]];
 
 
     $(".grid div").click(function(){
@@ -8,65 +8,96 @@ $(document).ready(function(){
         let marcaCasillero = casillero.text();
 
         let id = casillero.attr("id");
-        console.log(id);
 
         switch(id){
             case "AA":     
                 if(marcaCasillero == marcaNula){
                     casillero.text(marcaActual);
+                    grid[0][0] = marcaActual;                   
+
                     cambiarMarca();
+
                 }
             break;
             case "AB":
                 if(marcaCasillero == marcaNula){
                     casillero.text(marcaActual);
+                    grid[0][1] = marcaActual;
+
+                    
                     cambiarMarca();
                 }
             break;
             case "AC":
                 if(marcaCasillero == marcaNula){
                     casillero.text(marcaActual);
+                    grid[0][2] = marcaActual;
+
                     cambiarMarca();
                 }
             break;
             case "BA":
                 if(marcaCasillero == marcaNula){
                     casillero.text(marcaActual);
+                    grid[1][0] = marcaActual;
+
                     cambiarMarca();
                 }
             break;
             case "BB":
                 if(marcaCasillero == marcaNula){
                     casillero.text(marcaActual);
+                    grid[1][1] = marcaActual;
+
                     cambiarMarca();
                 }
             break;
             case "BC":
                 if(marcaCasillero == marcaNula){
                     casillero.text(marcaActual);
+                    grid[1][2] = marcaActual;
+
                     cambiarMarca();
                 }
             break;
             case "CA":
                 if(marcaCasillero == marcaNula){
                     casillero.text(marcaActual);
+                    grid[2][0] = marcaActual;
+
                     cambiarMarca();
                 }
             break;
             case "CB":
                 if(marcaCasillero == marcaNula){
                     casillero.text(marcaActual);
+                    grid[2][1] = marcaActual;
+
                     cambiarMarca();
                 }
             break;
             case "CC":
                 if(marcaCasillero == marcaNula){
                     casillero.text(marcaActual);
+                    grid[2][2] = marcaActual;
+
                     cambiarMarca();
                 }
-            break;
+            break;   
         }
 
-        //Corroborar si ganó o si se alcanzaron 9 clicks (no contar clicks en donde ya habia algo)
-    });
+        if(ganoX(grid)){
+            showWinMesssagge("X");
+        }
+
+        if(ganoO(grid)){
+            showWinMesssagge("O");
+        }
+
+
+        if((clickCounter == 9) && (ganoX(grid)==false) && (ganoO(grid)==false)){
+                showWinMesssagge("Empate");
+        }
+
+    }); //end Click
 })
